a61c8c093db84bcfb7ae1c1d1fbea1de657715d3
zhaofq <zhao921106@yeah.net>
2017-04-28 17:09:56 +0800

    。。。

--- app/src/main/java/gd/mytest1/GlideActivity.java
+++ app/src/main/java/gd/mytest1/GlideActivity.java
@@ -2,12 +2,27 @@ package gd.mytest1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 
 public class GlideActivity extends AppCompatActivity {
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_glide);
+        imageView = (ImageView) findViewById(R.id.image_view);
+    }
+
+    public void loadImage(View view) {
+
+        //        String url = "http://cn.bing.com/az/hprichbg/rb/Dongdaemun_ZH-CN10736487148_1920x1080.jpg";
+        String url = "http://p1.pstatp.com/large/166200019850062839d3";
+        Glide.with(this).load(url).placeholder(R.mipmap.ic_launcher).diskCacheStrategy(DiskCacheStrategy.NONE).into(imageView);
     }
 }
