e0c5cc9dcdbcdb47d3c387cf7510d5759b98105f
zhaofq <zhao921106@yeah.net>
2017-06-26 14:59:21 +0800

    git1

--- app/src/main/java/gd/mytest1/MainActivity.java
+++ app/src/main/java/gd/mytest1/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        System.out.print("git1");
     }
 
     public void toGlide(View view) {
